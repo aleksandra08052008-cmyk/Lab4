555c365e739c9938cc94d9b2fe7c72a1f76d5e0e
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-21 16:15:32 +0400

    Create Punctuation.java

---
+++ src/main/java/model/Punctuation.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Розділові знаки
+ */
+public class Punctuation {
+
+    private char symbol;
+
+    public Punctuation(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
