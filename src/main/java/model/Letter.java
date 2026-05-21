92f932e06b8089767b11d647abb4fa7dd4d96405
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-21 16:12:52 +0400

    Create Letter.java

---
+++ src/main/java/model/Letter.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Літера як окремий об’єкт
+ */
+public class Letter {
+
+    private char value;
+
+    public Letter(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+}
